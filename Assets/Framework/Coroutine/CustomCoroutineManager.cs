dc1740948faf65d1f9f6b6b103afd3812ada933e
Yang Liu <yang.liush@ef.com>
2020-01-19 15:36:40 +0800

    Add CustomCoroutineManager
    * clean up scripts of Easy Save plugin
    * add coroutine manager

---
+++ Assets/Framework/Coroutine/CustomCoroutineManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AKBFramework
+{
+    public class CustomCoroutineManager : MonoSingleton<CustomCoroutineManager>
+    {
+        private HashSet<CustomCoroutine> _coroutines = new HashSet<CustomCoroutine>();
+        
+        /// <summary>
+        /// convert coroutine invoked in CustomCoroutineManager to customCoroutine
+        /// </summary>
+        public new CustomCoroutine StartCoroutine(IEnumerator coroutine)
+        {
+            return this.StartCustomCoroutine(coroutine);
+        }
+
+        public void AddCoroutine(CustomCoroutine customCoroutine)
+        {
+            _coroutines.Add(customCoroutine);
+        }
+
+        public void RemoveCoroutine(CustomCoroutine customCoroutine)
+        {
+            _coroutines.Remove(customCoroutine);
+        }
+
+        /// <summary>
+        /// Stop all the custom coroutines
+        /// </summary>
+        public void Pause()
+        {
+            HashSet<CustomCoroutine>.Enumerator it = _coroutines.GetEnumerator();
+            while (it.MoveNext())
+            {
+                it.Current.Pause();
+            }
+        }
+
+        /// <summary>
+        /// Resume all the custom coroutines
+        /// </summary>
+        public void Resume()
+        {
+            HashSet<CustomCoroutine>.Enumerator it = _coroutines.GetEnumerator();
+            while (it.MoveNext())
+            {
+                it.Current.Resume();
+            }
+        }
+    }
+}
+
---
+++ Assets/Framework/Coroutine/CustomYieldInstruction/WaitForAnimatorInit.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace EFFramework
+{
+    public class WaitForAnimatorInit : CustomYieldInstruction
+    {
+        private Animator _animator;
+
+        public WaitForAnimatorInit(Animator animator)
+        {
+            _animator = animator;
+        }
+
+        // To keep coroutine suspended return true.
+        // To let coroutine proceed with execution return false.
+        // keepWaiting property is queried each frame after MonoBehaviour.Update and before MonoBehaviour.LateUpdate.
+        public override bool keepWaiting
+        {
+            get { return !_animator.isInitialized; }
+        }
+    }
+
+    public class WaitForAnimatorComplete : CustomYieldInstruction
+    {
+        private Animator _animator;
+        AnimatorStateInfo info;
+
+        public WaitForAnimatorComplete(Animator animator)
+        {
+            _animator = animator;
+            info = animator.GetCurrentAnimatorStateInfo(0);
+        }
+
+        // To keep coroutine suspended return true.
+        // To let coroutine proceed with execution return false.
+        // keepWaiting property is queried each frame after MonoBehaviour.Update and before MonoBehaviour.LateUpdate.
+        public override bool keepWaiting
+        {
+            get { 
+                Debug.Log(info.normalizedTime);
+                return info.normalizedTime < 1.0f; }
+        }
+    }
+}
